01da8ce247efea7800838baa769af7b8d4bd8af0
Carl Bordum Hansen <carl@bordum.dk>
2018-01-21 17:51:52 +0100

    fixed star alignment

--- src/lithp.h
+++ src/lithp.h
@@ -51,76 +51,77 @@ enum {
     LVAL_FUN, LVAL_SEXPR, LVAL_QEXPR,
 };
 
-lval* lval_err(char* fmt, ...);
-lval* lval_num(long x);
-lval* lval_sym(char* s);
-lval* lval_str(char* s);
-lval* lval_fun(lbuiltin func);
-lval* lval_lambda(lval* formals, lval* body);
-lval* lval_sexpr(void);
-lval* lval_qexpr(void);
 
-void lval_cleanup(lval* v);
-int lval_eq(lval* x, lval* y);
-lval* lval_copy(lval* v);
-lval* lval_pop(lval* v, int i);
-lval* lval_add(lval* v, lval* x);
-lval* lval_join(lval* x, lval* y);
-lval* lval_call(lenv* e, lval* f, lval* a);
-lval* lval_take(lval* v, int i);
-lval* lval_eval(lenv* e, lval* v);
-lval* lval_eval_sexpr(lenv* e, lval* v);
+lval *lval_err(char *fmt, ...);
+lval *lval_num(long x);
+lval *lval_sym(char *s);
+lval *lval_str(char *s);
+lval *lval_fun(lbuiltin func);
+lval *lval_lambda(lval *formals, lval *body);
+lval *lval_sexpr(void);
+lval *lval_qexpr(void);
 
-void lval_print(lval* v);
-void lval_println(lval* v);
-void lval_expr_print(lval* v, char open, char close);
-char* ltype_to_name(int type);
+void lval_cleanup(lval *v);
+int lval_eq(lval *x, lval *y);
+lval *lval_copy(lval *v);
+lval *lval_pop(lval *v, int i);
+lval *lval_add(lval *v, lval *x);
+lval *lval_join(lval *x, lval *y);
+lval *lval_call(lenv *e, lval *f, lval *a);
+lval *lval_take(lval *v, int i);
+lval *lval_eval(lenv *e, lval *v);
+lval *lval_eval_sexpr(lenv *e, lval *v);
 
-lval* lval_read(mpc_ast_t* t);
+void lval_print(lval *v);
+void lval_println(lval *v);
+void lval_expr_print(lval *v, char open, char close);
+char *ltype_to_name(int type);
+
+lval *lval_read(mpc_ast_t *t);
 
 
-lenv* lenv_new(void);
-void lenv_put(lenv* e, lval* k, lval* v);
-void lenv_put_global(lenv* e, lval* k, lval* v);
-lval* lenv_get(lenv* e, lval* k);
-void lenv_clean_up(lenv* e);
-lenv* lenv_copy(lenv* e);
+lenv *lenv_new(void);
+void lenv_put(lenv *e, lval *k, lval *v);
+void lenv_put_global(lenv *e, lval *k, lval *v);
+lval *lenv_get(lenv *e, lval *k);
+void lenv_clean_up(lenv *e);
+lenv *lenv_copy(lenv *e);
 
 
 /* BUILTINS */
-lval* builtin_list(lenv* e, lval* v);
-lval* builtin_head(lenv* e, lval* v);
-lval* builtin_tail(lenv* e, lval* v);
-lval* builtin_eval(lenv* e, lval* v);
-lval* builtin_join(lenv* e, lval* v);
-lval* builtin_def(lenv* e, lval* v);
-lval* builtin_lambda(lenv* e, lval* v);
-lval* builtin_put(lenv* e, lval* v);
+lval *builtin_list(lenv *e, lval *v);
+lval *builtin_head(lenv *e, lval *v);
+lval *builtin_tail(lenv *e, lval *v);
+lval *builtin_eval(lenv *e, lval *v);
+lval *builtin_join(lenv *e, lval *v);
+lval *builtin_def(lenv *e, lval *v);
+lval *builtin_lambda(lenv *e, lval *v);
+lval *builtin_put(lenv *e, lval *v);
 
-lval* builtin_add(lenv* e, lval* v);
-lval* builtin_sub(lenv* e, lval* v);
-lval* builtin_mul(lenv* e, lval* v);
-lval* builtin_div(lenv* e, lval* v);
+lval *builtin_add(lenv *e, lval *v);
+lval *builtin_sub(lenv *e, lval *v);
+lval *builtin_mul(lenv *e, lval *v);
+lval *builtin_div(lenv *e, lval *v);
 
-lval* builtin_if(lenv* e, lval* v);
-lval* builtin_eq(lenv* e, lval* v);
-lval* builtin_ne(lenv* e, lval* v);
-lval* builtin_gt(lenv* e, lval* v);
-lval* builtin_lt(lenv* e, lval* v);
-lval* builtin_ge(lenv* e, lval* v);
-lval* builtin_le(lenv* e, lval* v);
+lval *builtin_if(lenv *e, lval *v);
+lval *builtin_eq(lenv *e, lval *v);
+lval *builtin_ne(lenv *e, lval *v);
+lval *builtin_gt(lenv *e, lval *v);
+lval *builtin_lt(lenv *e, lval *v);
+lval *builtin_ge(lenv *e, lval *v);
+lval *builtin_le(lenv *e, lval *v);
 
-lval* builtin_import(lenv* e, lval* v);
-lval* builtin_print(lenv* e, lval* v);
-lval* builtin_error(lenv* e, lval* v);
+lval *builtin_import(lenv *e, lval *v);
+lval *builtin_print(lenv *e, lval *v);
+lval *builtin_error(lenv *e, lval *v);
 
 
 /* PARSER */
-mpc_parser_t* Number;
-mpc_parser_t* String;
-mpc_parser_t* Comment;
-mpc_parser_t* Symbol;
-mpc_parser_t* Sexpr;
-mpc_parser_t* Qexpr;
-mpc_parser_t* Expr;
-mpc_parser_t* Program;
+mpc_parser_t *Number;
+mpc_parser_t *String;
+mpc_parser_t *Comment;
+mpc_parser_t *Symbol;
+mpc_parser_t *Sexpr;
+mpc_parser_t *Qexpr;
+mpc_parser_t *Expr;
+mpc_parser_t *Program;
